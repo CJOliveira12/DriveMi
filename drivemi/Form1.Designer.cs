9e176abb912b66bc9d97ee47e19bb18d2fcc2d6c
Ricardo Tavares <ricardo@ricardomnt.com>
2016-11-14 19:39:40 +0000

    Add map view

--- drivemi/Form1.Designer.cs
+++ drivemi/Form1.Designer.cs
@@ -29,164 +29,87 @@
         private void InitializeComponent()
         {
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-            this.inicioToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
             this.lblSearch = new System.Windows.Forms.Label();
             this.radioButton1 = new System.Windows.Forms.RadioButton();
             this.radioButton2 = new System.Windows.Forms.RadioButton();
             this.radioButton3 = new System.Windows.Forms.RadioButton();
-            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.gMapControl = new GMap.NET.WindowsForms.GMapControl();
             this.lblRadius = new System.Windows.Forms.Label();
             this.trackBar1 = new System.Windows.Forms.TrackBar();
             this.lblZoom = new System.Windows.Forms.Label();
-            this.trackBar2 = new System.Windows.Forms.TrackBar();
-            this.lblMsgLength = new System.Windows.Forms.Label();
-            this.txtMessage = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.txtNumber = new System.Windows.Forms.TextBox();
-            this.gMapControl = new GMap.NET.WindowsForms.GMapControl();
+            this.trackBarZoom = new System.Windows.Forms.TrackBar();
             this.menuStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarZoom)).BeginInit();
             this.SuspendLayout();
             // 
             // menuStrip1
             // 
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.inicioToolStripMenuItem});
+            this.toolStripMenuItem1});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(1083, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(887, 24);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
-            // inicioToolStripMenuItem
+            // toolStripMenuItem1
             // 
-            this.inicioToolStripMenuItem.Name = "inicioToolStripMenuItem";
-            this.inicioToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
-            this.inicioToolStripMenuItem.Text = "Inicio";
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(48, 20);
+            this.toolStripMenuItem1.Text = "Inicio";
             // 
             // lblSearch
             // 
             this.lblSearch.AutoSize = true;
-            this.lblSearch.Location = new System.Drawing.Point(13, 39);
+            this.lblSearch.Location = new System.Drawing.Point(22, 46);
+            this.lblSearch.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblSearch.Name = "lblSearch";
             this.lblSearch.Size = new System.Drawing.Size(59, 13);
-            this.lblSearch.TabIndex = 1;
+            this.lblSearch.TabIndex = 503;
             this.lblSearch.Text = "Search for:";
             // 
             // radioButton1
             // 
             this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(16, 56);
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(25, 72);
+            this.radioButton1.Margin = new System.Windows.Forms.Padding(2);
             this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(41, 17);
-            this.radioButton1.TabIndex = 2;
+            this.radioButton1.Size = new System.Drawing.Size(36, 17);
+            this.radioButton1.TabIndex = 504;
             this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "Car";
+            this.radioButton1.Text = "All";
             this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
             // 
             // radioButton2
             // 
             this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(165, 55);
+            this.radioButton2.Location = new System.Drawing.Point(65, 72);
+            this.radioButton2.Margin = new System.Windows.Forms.Padding(2);
             this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(44, 17);
-            this.radioButton2.TabIndex = 3;
-            this.radioButton2.TabStop = true;
-            this.radioButton2.Text = "Van";
+            this.radioButton2.Size = new System.Drawing.Size(41, 17);
+            this.radioButton2.TabIndex = 505;
+            this.radioButton2.Tag = "Car";
+            this.radioButton2.Text = "Car";
             this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
             // 
             // radioButton3
             // 
             this.radioButton3.AutoSize = true;
-            this.radioButton3.Location = new System.Drawing.Point(63, 55);
+            this.radioButton3.Location = new System.Drawing.Point(110, 72);
+            this.radioButton3.Margin = new System.Windows.Forms.Padding(2);
             this.radioButton3.Name = "radioButton3";
-            this.radioButton3.Size = new System.Drawing.Size(96, 17);
-            this.radioButton3.TabIndex = 4;
-            this.radioButton3.TabStop = true;
-            this.radioButton3.Text = "Station Wagon";
+            this.radioButton3.Size = new System.Drawing.Size(44, 17);
+            this.radioButton3.TabIndex = 506;
+            this.radioButton3.Tag = "Van";
+            this.radioButton3.Text = "Van";
             this.radioButton3.UseVisualStyleBackColor = true;
-            // 
-            // radioButton4
-            // 
-            this.radioButton4.AutoSize = true;
-            this.radioButton4.Location = new System.Drawing.Point(215, 56);
-            this.radioButton4.Name = "radioButton4";
-            this.radioButton4.Size = new System.Drawing.Size(44, 17);
-            this.radioButton4.TabIndex = 5;
-            this.radioButton4.TabStop = true;
-            this.radioButton4.Text = "Suv";
-            this.radioButton4.UseVisualStyleBackColor = true;
-            // 
-            // lblRadius
-            // 
-            this.lblRadius.AutoSize = true;
-            this.lblRadius.Location = new System.Drawing.Point(12, 85);
-            this.lblRadius.Name = "lblRadius";
-            this.lblRadius.Size = new System.Drawing.Size(72, 13);
-            this.lblRadius.TabIndex = 6;
-            this.lblRadius.Text = "Radius: 500m";
-            // 
-            // trackBar1
-            // 
-            this.trackBar1.Location = new System.Drawing.Point(12, 103);
-            this.trackBar1.Name = "trackBar1";
-            this.trackBar1.Size = new System.Drawing.Size(244, 45);
-            this.trackBar1.TabIndex = 7;
-            // 
-            // lblZoom
-            // 
-            this.lblZoom.AutoSize = true;
-            this.lblZoom.Location = new System.Drawing.Point(13, 151);
-            this.lblZoom.Name = "lblZoom";
-            this.lblZoom.Size = new System.Drawing.Size(52, 13);
-            this.lblZoom.TabIndex = 8;
-            this.lblZoom.Text = "Zoom: 14";
-            // 
-            // trackBar2
-            // 
-            this.trackBar2.Location = new System.Drawing.Point(16, 167);
-            this.trackBar2.Name = "trackBar2";
-            this.trackBar2.Size = new System.Drawing.Size(243, 45);
-            this.trackBar2.TabIndex = 9;
-            // 
-            // lblMsgLength
-            // 
-            this.lblMsgLength.AutoSize = true;
-            this.lblMsgLength.Location = new System.Drawing.Point(574, 213);
-            this.lblMsgLength.Name = "lblMsgLength";
-            this.lblMsgLength.Size = new System.Drawing.Size(36, 13);
-            this.lblMsgLength.TabIndex = 10;
-            this.lblMsgLength.Text = "0/160";
-            // 
-            // txtMessage
-            // 
-            this.txtMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
-            this.txtMessage.Location = new System.Drawing.Point(577, 45);
-            this.txtMessage.Margin = new System.Windows.Forms.Padding(2);
-            this.txtMessage.Multiline = true;
-            this.txtMessage.Name = "txtMessage";
-            this.txtMessage.Size = new System.Drawing.Size(299, 166);
-            this.txtMessage.TabIndex = 11;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(274, 39);
-            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(73, 13);
-            this.label3.TabIndex = 517;
-            this.label3.Text = "From Number:";
-            // 
-            // txtNumber
-            // 
-            this.txtNumber.Location = new System.Drawing.Point(277, 56);
-            this.txtNumber.Margin = new System.Windows.Forms.Padding(2);
-            this.txtNumber.Name = "txtNumber";
-            this.txtNumber.Size = new System.Drawing.Size(221, 20);
-            this.txtNumber.TabIndex = 518;
-            this.txtNumber.Text = "+351999999999";
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
             // 
             // gMapControl
             // 
@@ -200,7 +123,7 @@
             this.gMapControl.GrayScaleMode = false;
             this.gMapControl.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
             this.gMapControl.LevelsKeepInMemmory = 5;
-            this.gMapControl.Location = new System.Drawing.Point(28, 239);
+            this.gMapControl.Location = new System.Drawing.Point(25, 125);
             this.gMapControl.Margin = new System.Windows.Forms.Padding(2);
             this.gMapControl.MarkersEnabled = true;
             this.gMapControl.MaxZoom = 17;
@@ -214,25 +137,64 @@
             this.gMapControl.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
             this.gMapControl.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
             this.gMapControl.ShowTileGridLines = false;
-            this.gMapControl.Size = new System.Drawing.Size(1026, 256);
-            this.gMapControl.TabIndex = 519;
+            this.gMapControl.Size = new System.Drawing.Size(823, 362);
+            this.gMapControl.TabIndex = 508;
             this.gMapControl.Zoom = 14D;
+            // 
+            // lblRadius
+            // 
+            this.lblRadius.AutoSize = true;
+            this.lblRadius.Location = new System.Drawing.Point(186, 46);
+            this.lblRadius.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblRadius.Name = "lblRadius";
+            this.lblRadius.Size = new System.Drawing.Size(72, 13);
+            this.lblRadius.TabIndex = 509;
+            this.lblRadius.Text = "Radius: 500m";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.LargeChange = 500;
+            this.trackBar1.Location = new System.Drawing.Point(189, 72);
+            this.trackBar1.Margin = new System.Windows.Forms.Padding(2);
+            this.trackBar1.Maximum = 15000;
+            this.trackBar1.Minimum = 500;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(292, 45);
+            this.trackBar1.SmallChange = 500;
+            this.trackBar1.TabIndex = 510;
+            this.trackBar1.TickFrequency = 500;
+            this.trackBar1.Value = 710;
+            // 
+            // lblZoom
+            // 
+            this.lblZoom.AutoSize = true;
+            this.lblZoom.Location = new System.Drawing.Point(502, 46);
+            this.lblZoom.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblZoom.Name = "lblZoom";
+            this.lblZoom.Size = new System.Drawing.Size(52, 13);
+            this.lblZoom.TabIndex = 512;
+            this.lblZoom.Text = "Zoom: 14";
+            // 
+            // trackBarZoom
+            // 
+            this.trackBarZoom.LargeChange = 2;
+            this.trackBarZoom.Location = new System.Drawing.Point(505, 72);
+            this.trackBarZoom.Margin = new System.Windows.Forms.Padding(2);
+            this.trackBarZoom.Maximum = 18;
+            this.trackBarZoom.Name = "trackBarZoom";
+            this.trackBarZoom.Size = new System.Drawing.Size(292, 45);
+            this.trackBarZoom.TabIndex = 513;
+            this.trackBarZoom.TickStyle = System.Windows.Forms.TickStyle.TopLeft;
+            this.trackBarZoom.Value = 14;
             // 
             // frmApp
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1083, 507);
-            this.Controls.Add(this.gMapControl);
-            this.Controls.Add(this.txtNumber);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.txtMessage);
-            this.Controls.Add(this.lblMsgLength);
-            this.Controls.Add(this.trackBar2);
+            this.ClientSize = new System.Drawing.Size(887, 498);
+            this.Controls.Add(this.trackBarZoom);
             this.Controls.Add(this.lblZoom);
             this.Controls.Add(this.trackBar1);
             this.Controls.Add(this.lblRadius);
-            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.gMapControl);
             this.Controls.Add(this.radioButton3);
             this.Controls.Add(this.radioButton2);
             this.Controls.Add(this.radioButton1);
@@ -240,11 +202,10 @@
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "frmApp";
-            this.Text = "DriveMi";
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarZoom)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -253,21 +214,17 @@
         #endregion
 
         private System.Windows.Forms.MenuStrip menuStrip1;
-        private System.Windows.Forms.ToolStripMenuItem inicioToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
         private System.Windows.Forms.Label lblSearch;
         private System.Windows.Forms.RadioButton radioButton1;
         private System.Windows.Forms.RadioButton radioButton2;
         private System.Windows.Forms.RadioButton radioButton3;
-        private System.Windows.Forms.RadioButton radioButton4;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private GMap.NET.WindowsForms.GMapControl gMapControl;
         private System.Windows.Forms.Label lblRadius;
         private System.Windows.Forms.TrackBar trackBar1;
         private System.Windows.Forms.Label lblZoom;
-        private System.Windows.Forms.TrackBar trackBar2;
-        private System.Windows.Forms.Label lblMsgLength;
-        private System.Windows.Forms.TextBox txtMessage;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox txtNumber;
-        private GMap.NET.WindowsForms.GMapControl gMapControl;
+        private System.Windows.Forms.TrackBar trackBarZoom;
     }
 }
 
--- drivemi/Form1.cs
+++ drivemi/Form1.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,11 +14,70 @@ namespace drivemi
 {
     public partial class frmApp : Form
     {
+        double lat = 23.179357;
+        double lng = 2;
+        public static List<ResourceModel> model { get; private set; }
+
         public frmApp()
         {
-            static double lat = 23.179357;
-            static double lng = 75.7827451;
+            
             InitializeComponent();
+
+            if (radioButton3.Checked)
+            {
+                drawMap(trackBar1.Value);
+            }
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                UpdateMap(trackBar1.Value);
+            }
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton2.Checked)
+            {
+                UpdateMap(trackBar1.Value);
+            }
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton3.Checked)
+            {
+                UpdateMap(trackBar1.Value);
+            }
+        }
+
+        private async void UpdateMap(int radius)
+        {
+            await fetchDetails(radius);
+            markPins(radius);
+        }
+        
+        private async Task fetchDetails (int radius)
+        {
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri("http://drivemi.dx.am/");
+                    client.DefaultRequestHeaders.Accept.Clear();
+                    client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("applocation/json"));
+
+                    HttpResponseMessage response = await client.GetAsync("api/resource?latitude=" + gMapControl.Position.Lat + "&longitude=" + gMapControl.Position.Lng + "&radius=" + trackBar1.Value.ToString());
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var jsonArray = await response.Content.ReadAsStringAsync();
+                        model = JsonConvert.DeserializeObject<List<ResourceModel>>(jsonArray);
+                    }
+                }
+            }
         }
     }
 }
